fcf6c2283056524e46f4458034987ab2442c1354
KanielPinto <kaniel.pinto@xaviers.edu.in>
2024-03-16 01:36:29 +0530

    news

--- src/app/dashboards/my-wheel/page.tsx
+++ src/app/dashboards/my-wheel/page.tsx
@@ -1,9 +1,17 @@
-
+import MySunburstChart from "@/components/MySunburstChart";
+import mydata from "./portfolio.js";
 
 export default function MyWheel() {
     return (
         <>
-            <h1 className="w-full py-4 px-6 text-3xl font-bold">My Wheel</h1>
+            <div className='flex flex-col justify-center'>
+                <h1 className="w-fit py-4 px-6 text-3xl font-bold">My Wheel</h1>
+                <div className="flex flex-row w-full">
+                    <MySunburstChart data={mydata} amount={150000} />
+                    {/* <MySunburstChart data={mydata} /> */}
+                </div>
+
+            </div>        
         </>
     )
 }
