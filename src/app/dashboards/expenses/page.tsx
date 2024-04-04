d5c3f1862318f4b76ae71327864747346dff45ef
Valeron <valerontoscano@gmail.com>
2024-04-04 22:36:36 +0530

    added datagrid

--- src/app/dashboards/expenses/page.tsx
+++ src/app/dashboards/expenses/page.tsx
@@ -1,13 +1,41 @@
+'use client'
 import UploadForm from '@/components/UploadForm'
-import React from 'react'
+import { useUser } from '@clerk/nextjs';
+import React, { useEffect, useState } from 'react'
 
 function page() {
+
+    const {user} = useUser();
+
+    const [transactions, setTransactions] = useState(null);
+
+    useEffect(() => {
+        () => {
+            fetch(`http://localhost:5000/transactions/${user?.id}`).then(async transactions => console.log(await transactions.json()))
+        }
+        console.log(transactions)
+    }, []);
+
+
+    const columns = [
+        {
+            field: 'id',
+            headerName: 'ID',
+            minWidth: 50,
+            headerAlign: 'center',
+        },
+        { field: 'transactions_name', headerName: 'Name', minWidth: 50 },
+    ]
+
+
     return (
         <div className='flex flex-col'>
             <h1 className="w-full py-4 text-3xl font-bold">Expense Tracking</h1>
             <div className="flex flex-row p-6">
                 <UploadForm />
             </div>
+
+
 
         </div>
     )
