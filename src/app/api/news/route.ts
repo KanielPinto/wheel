5e203e227d531c6e42662aa9b46627d18cb14cf6
KanielPinto <kaniel.pinto@xaviers.edu.in>
2024-04-05 09:04:57 +0530

    remove unnecessary api

--- src/app/api/news/route.ts
+++
@@ -1,7 +0,0 @@
-export async function GET(request: Request) {
-    const res = await fetch('http://127.0.0.1:5000/news/')
-
-    const news = await res.json()
-   
-    return Response.json({ news })
-  }
