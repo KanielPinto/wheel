935da4241a954330dc50baaaa1a02107a95ebeea
KanielPinto <kaniel.pinto@xaviers.edu.in>
2024-04-09 20:15:36 +0530

    sign in sign out refinement

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,4 +1,5 @@
 import { ClerkLoading, SignIn } from "@clerk/nextjs";
+import { Spinner } from "@nextui-org/react";
 import Link from "next/link";
 
 export default function Page() {
@@ -7,14 +8,16 @@ export default function Page() {
             <div className="h-full w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
                 {/* Radial gradient for the container to give a faded look */}
                 <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-                <div className="flex h-screen w-full justify-center items-center flex-col"><ClerkLoading>
-                    <div>Clerk is loading</div>
-                </ClerkLoading>
+                <div className="flex h-screen gap-2 w-full justify-center items-center flex-col">
+                    <ClerkLoading>
+                        <Spinner size="lg" color="secondary" />
+                        <div>Clerk is loading...</div>
+                    </ClerkLoading>
                     <SignIn></SignIn>
                     <div className="text-left py-2">
-                        <p>Forgot Password? 
+                        <p>Forgot Password?
                             <Link href={"/forgot-password"} className="text-blue-400 underline mx-1">
-                                 Click Here
+                                Click Here
                             </Link>
                         </p>
                     </div>
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,11 +1,18 @@
-import { SignUp } from "@clerk/nextjs";
+import { ClerkLoading, SignUp } from "@clerk/nextjs";
+import { Spinner } from "@nextui-org/react";
 
 export default function Page() {
     return (
         <div className="h-full w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
             {/* Radial gradient for the container to give a faded look */}
             <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-            <div className="flex h-screen w-full justify-center items-center"><SignUp /></div>
+            <div className="flex flex-col gap-2 h-screen w-full justify-center items-center">
+                <ClerkLoading>
+                    <Spinner size="lg" color="secondary" />
+                    <div>Clerk is loading...</div>
+                </ClerkLoading>
+                <SignUp />
+            </div>
 
         </div>
     );
